e28f61efdf65df48b85a8c4ca477586c37c854ed
Mauro Pelucchi <mauro.pelucchi@gmail.com>
2025-04-21 09:03:33 +0200

    update talk.js

--- api/Get_Talks_by_Tag/Talk.js
+++ api/Get_Talks_by_Tag/Talk.js
@@ -4,7 +4,8 @@ const talk_schema = new mongoose.Schema({
     title: String,
     url: String,
     description: String,
-    speakers: String
+    speakers: String,
+    comprehend_analysis: mongoose.Schema.Types.Mixed
 }, { collection: 'tedx_data' });
 
 module.exports = mongoose.model('talk', talk_schema);
